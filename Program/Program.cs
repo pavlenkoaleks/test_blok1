4ccfd17a365ecd30ad7e16b6724bf6dda67897c3
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-07-10 15:14:36 +0500

    дописал код, добавил провер ку на длину элеметов массива

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,11 +1,20 @@
-﻿
+﻿using System;
 Console.Clear();
 Console.Write("Введите исходный массив строк(элементы через пробел) : ");
 string str = Console.ReadLine();
 Console.WriteLine(str);
 string[] words = str.Split(' ');
+Console.WriteLine("Введенные значения: ");
 foreach (var word in words)
 {
-    System.Console.WriteLine($"<{word}>");
+    Console.WriteLine($"<{word}>");
 }
+string[] result = new string[100];
+int count=0;
+for(int i=0;i<words.Length;i++){
+  if (words[i].Length<=3) {result[count]=words[i];count++;}
+}
+ Console.WriteLine("Длина элементов <=3 : ");
+ Console.WriteLine(String.Join(" ",result));
 
+ 
