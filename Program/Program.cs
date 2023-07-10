17f7c1d24d16f01256f0f5a288e718e003a5fb23
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-07-10 13:47:09 +0500

    создал проект, инициализировал массив, распарсил строку

---
+++ Program/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Clear();
+Console.Write("Введите исходный массив строк(элементы через пробел) : ");
+string str = Console.ReadLine();
+Console.WriteLine(str);
+string[] words = str.Split(' ');
+foreach (var word in words)
+{
+    System.Console.WriteLine($"<{word}>");
+}
+
